ce9a738b065307443683cfcefeeb404c83a6af1b
sysoevss <sysoev@petroms.ru>
2019-11-25 12:16:54 +0300

    Delete initial_databases.sql

--- mysql/initial_databases.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE spa;
-USE spa;
-
-CREATE TABLE users ( id INT unsigned not null auto_increment,
-					 user_name VARCHAR(30) not null,
-					 password VARCHAR(30) not null,
-					 constraint pk_example primary key (id) );
-INSERT INTO users ( user_name, password ) VALUES ( "sysoev", "123" );
